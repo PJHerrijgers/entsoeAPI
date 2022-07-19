f1d1ddc8489ff3e0f6ddeab99c1904028645cbb8
PJHerrijgers <pieterjan.herrijgers@student.kuleuven.be>
2022-07-19 09:48:40 +0200

    first data

--- src/entsoeAPI.jl
+++ src/entsoeAPI.jl
@@ -1,5 +1,11 @@
-module entsoeAPI
+using HTTP
 
-print("Hello world")
+URL = "https://transparency.entsoe.eu/api"
 
+function test(key)
+    response = HTTP.request("GET", URL*"?securityToken="*key*"&documentType=A65&processType=A16&outBiddingZone_Domain=10YCZ-CEPS-----N&periodStart=201512312300&periodEnd=201612312300")
+    return response
 end
+
+print(test("6e9d0b18-9bde-41cf-938f-c8ad9b35d97d"))
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using entsoeAPI
 using Test
 
-@testset "entsoeAPI.jl" begin
-    # Write your tests here.
-end
+#= @testset "entsoeAPI.jl" begin
+
+end =#
+
+API-key = "6e9d0b18-9bde-41cf-938f-c8ad9b35d97d"
+data = entsoeAPI.test(API-key)
+print(data)
