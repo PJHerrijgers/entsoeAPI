bc1a141dc15fad0a16003d8a5725796be4d4e531
PJHerrijgers <pieterjan.herrijgers@student.kuleuven.be>
2022-07-22 17:30:22 +0200

    country mapping + get function till master data

---
+++ src/argumentLimitations.jl
@@ -0,0 +1,11 @@
+module argumentLimitations
+
+FlowbasedProcessType = Set(["A01", "A02"])
+eaiCapacityBusinessType = Set(["A43", "B05"])
+ianpBusinessType = Set(["B09", "B10"])
+SchedulesContractType = Set(["A05", "A01"])
+
+ExpansionBusinessType = Set(["B01", "B02", ""])
+redispatchingBusinessType = Set(["A46", "A85", ""])
+congestionBusinessType = Set(["A46", "B03", "B04", ""])
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 include("../src/GETconstructor.jl")
+include("../src/mappings.jl")
 using .GETconstructor
+using .mappings
 using Test
 using HTTP
 
@@ -14,8 +16,9 @@ APIkey = "6e9d0b18-9bde-41cf-938f-c8ad9b35d97d"
     end
 end
 
-GETconstructor.initialize_key(APIkey)
+#= 
 
+GETconstructor.initialize_key(APIkey)
 open("data/load_actual_test.txt", "w") do f
     write(f, GETconstructor.query_actual_total_load("10YCZ-CEPS-----N", "201512312300", "201612312300"))
 end
@@ -38,4 +41,28 @@ end
 
 open("data/margin_year_ahead_test.txt", "w") do f
     write(f, GETconstructor.query_year_ahead_margin("10YCZ-CEPS-----N", "201512312300", "201612312300"))
+end 
+=#
+open("data/flowbased.txt", "w") do f
+    write(f, GETconstructor.query_flowbased("A01", "10YDOM-REGION-1V", "10YDOM-REGION-1V", "201512312300", "201601012300"))
 end
+
+
+
+################ test mappings ######################"
+
+@testset verbose = true "mappings.jl" begin
+    @testset "mapping with country code" begin
+        @test mappings.lookup_area("NO") == mappings.NO
+        @test mappings.lookup_area("BE") == mappings.BE
+    end
+    @testset "mapping with Area object" begin
+        @test mappings.lookup_area(mappings.NO) == mappings.NO
+        @test mappings.lookup_area(mappings.BE) == mappings.BE
+    end
+    @testset "mapping with direct code" begin
+        @test mappings.lookup_area("10YBE----------2") == mappings.BE
+        @test mappings.lookup_area("10YNO-0--------C") == mappings.NO
+    end
+end
+
